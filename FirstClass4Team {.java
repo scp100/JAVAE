27a82a19de3cbaa70cebf1da1cb763c4b607a881
Yunki746630 <139089337+Yunki746630@users.noreply.github.com>
2025-04-29 21:18:29 +0800

    Update FirstClass4Team {.java
    加入我的學號及姓名

--- FirstClass4Team {.java
+++ FirstClass4Team {.java
@@ -1,7 +1,6 @@
-public class FirstClass4Team {
-    public static void main(String[] args) {    
+public static void main(String[] args){    
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01057043\"\n姓名：\"施又誠\"");
-    //
-    }
+    System.out.println("\"學號：\"01057149\"\n姓名：\"廖宏騰\"");
+    System.out.println("\"學號：\"01057102\"\n姓名：\"潘昀\"");
 }
