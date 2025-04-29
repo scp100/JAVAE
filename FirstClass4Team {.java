35439bf23c83dc5360a9987158a1183426cda655
yarn5229 <125251068+yarn5229@users.noreply.github.com>
2025-04-29 23:32:26 +0800

    add 學號 姓名

--- FirstClass4Team {.java
+++ FirstClass4Team {.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {    
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01057043\"\n姓名：\"施又誠\"");
+    System.out.println("\"學號：\"01057149\"\n姓名：\"廖宏騰\"");
     //
     }
 }
